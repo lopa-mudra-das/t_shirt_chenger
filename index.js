445d0b7842f5b94d9d709e693d4e2b404515ef2f
lopa-mudra-das <lopamudradas459@gmail.com>
2023-02-03 11:50:47 +0530

    t_shirt

---
+++ index.js
@@ -0,0 +1,16 @@
+var arr=["img2.jpg","img3.jpg","img4.jpg","img5.webp","img6.jpg","img7.jpg","img8.jpg","img9.jpg","img10.jpg","img11.jpg","img12.jpg"];
+   var i=0;
+   let play=true
+   function displya() {
+    var im= document.querySelector("img")
+     im.src="/matching_profile/t_shirt/t_shirt_chenger/images/"+arr[i];
+     i++;
+     if(i==arr.length)//i==4
+     {
+      i=0;
+     }
+   }     
+   function clk(){
+    document.getElementById("ply").src="/matching_profile/t_shirt/t_shirt_chenger/images/circled-pause.png"
+    setInterval(displya,1000)
+   }
